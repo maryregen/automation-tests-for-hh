aa563181e8fb904db5613b6ee745e42ede613140
Maria Goncharova <maryregen@yandex.ru>
2023-04-16 18:29:54 +0400

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Выбираем опции Gradle в зависимости от типа запускаемых тестов
+# - API-тесты запускаем без параметризации
+# - UI-тесты запускаем с передачей BROWSER_PLATFORM, BROWSER_VERSION, BROWSER_SIZE, REMOTE_URL
+# - Mobile-тесты запускаем с deviceHost (browserstack)
+#
+# Например:
+#
+# ./gradlew clean api_tests
+# ./gradlew clean ui_tests -Dbrowser=CHROME -Dversion=112.0 -DwindowSize=1920x1080 -DremoteUrl=http://localhost:4444/wd/hub
+# ./gradlew clean mobile_tests -DdeviceHost=browserstack
+
+gradle_opts=""
+
+if [[ -z $TEST_TYPE ]] ; then
+  echo "Error: TEST_TYPE must be specified" >&2
+  exit 1
+fi
+
+case "$TEST_TYPE" in 
+  "ui_tests")
+    BROWSER_PLATFORM=$(echo "${BROWSER}" | awk '{print $1}')
+    BROWSER_VERSION=$(echo "${BROWSER}" | awk '{print $2}')
+
+    gradle_opts="-Dbrowser=${BROWSER_PLATFORM} -Dversion=${BROWSER_VERSION} -DwindowSize=${BROWSER_SIZE} -DremoteUrl=${REMOTE_URL}"
+  ;;
+  "mobile_tests")
+    gradle_opts="-DdeviceHost=browserstack"
+  ;;
+  "api_tests")
+  ;;
+esac
+
+if [ -n $gradle_opts ] ; then
+  ./gradlew clean "$TEST_TYPE" "$gradle_opts"
+else
+  ./gradlew clean "$TEST_TYPE"
+fi
